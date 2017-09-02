d7c6d4ee5a1dbd43a50fd2963d0d5f19784b56aa
Alan Griggs <agriggs@ieee.org>
2017-09-01 23:37:07 -0500

    Additional ini_set settings for good cookie hygiene, lifetime, garbage collection, and httpOnly

--- org.php
+++ org.php
@@ -1,7 +1,10 @@
 <?php
 ini_set('display_errors', 'On');
 ini_set('session.cookie_httponly', '1');
-ini_set('session.cookie_lifetime', 7200);
+ini_set('session.cookie_lifetime', '7200');
+ini_set('session.gc_maxlifetime', '86400');
+ini_set('session.gc_probability', '1');
+
 error_reporting(E_ALL | E_STRICT);
 
 require_once('include/secrets.php');
