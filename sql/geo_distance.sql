8700945bb0e6f03289959053c7da9b7cd64c2207
Alan Griggs <agriggs@ieee.org>
2017-10-09 19:32:47 +0000

    Used proper distance function from mysql 5.7

--- sql/geo_distance.sql
+++ sql/geo_distance.sql
@@ -1,20 +1,16 @@
+DROP FUNCTION IF EXISTS geo_distance;
+
 DELIMITER $$
 CREATE FUNCTION geo_distance (lat1 double, long1 double, lat2 double, long2 double)
 RETURNS double DETERMINISTIC
-
 BEGIN
-	DECLARE lat_diff double;
-	DECLARE long_diff double;
-	DECLARE lat_dist double;
-	DECLARE long_dist double;
-	DECLARE dist double;
-	SET lat_diff = lat1 - lat2;
-	SET lat_dist = 111 * lat_diff;
-	SET long_diff = long1 - long2;
-	SET long_dist = (111 * COS(long1 / 2 * PI())) * long_diff;
-	SET dist = SQRT(POW(long_dist,2) + POW(lat_dist,2));
-RETURN dist;
-END$$
+	DECLARE pt1 POINT;
+	DECLARE pt2 POINT;
+	DECLARE dist DOUBLE;
+	SET pt1 = POINT(long1, lat1);
+	SET pt2 = POINT(long2, lat2);
+	SET dist = ST_Distance_Sphere(pt1, pt2)/1000;
+RETURN dist;$$
 DELIMITER ;
 
 GRANT EXECUTE ON FUNCTION geo_distance TO movemusr@localhost;
