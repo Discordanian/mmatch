bcb89efce527d0c910784979bbbf69276e6aa81a
Kurt Schwind <kurt.schwind@gmail.com>
2017-11-03 23:57:23 +0000

    Removing STL from title page on index and match

--- match.php
+++ match.php
@@ -46,7 +46,7 @@ $mconfig['answers'] = getAnswers($mconfig['jsondata']);
 <html >
 <head>
   <meta charset="UTF-8">
-  <title>STL MM Search Demo</title>
+  <title>Movement Match</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   
 <link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css'>
