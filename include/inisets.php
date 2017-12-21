77e3a514e24a39fd1d8cc08510bd7d682aeb647c
Alan Griggs <agriggs@ieee.org>
2017-12-21 10:13:16 -0600

    Set cookie_secure to true

--- include/inisets.php
+++ include/inisets.php
@@ -5,10 +5,12 @@ ini_set('session.cookie_httponly', 'On');
 ini_set('session.cookie_lifetime', '14400');
 ini_set('session.gc_maxlifetime', '86400');
 ini_set('session.gc_probability', '1');
+ini_set('session.cookie_secure', 'On');
 
 error_reporting(E_ALL | E_STRICT);
 
 define("DUPLICATE_ORG_NAME_ERROR", "The organization name entered was a duplicate.");
+define("USER_NOT_LOGGED_IN_ERROR", "The user must log in to perform this function. Please log in.");
 
 ?>
 
