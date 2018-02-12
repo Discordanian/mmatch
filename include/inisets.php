9dbf054e13822ce3e7c69f9f04e3ac9f5db18585
Alan Griggs <agriggs@ieee.org>
2018-02-12 00:16:06 +0000

    Corrected secure cookie attribute in inisets.php

--- include/inisets.php
+++ include/inisets.php
@@ -1,11 +1,11 @@
 <?php
 
-ini_set('display_errors', 'On');
+ini_set('display_errors', 'Off');
 ini_set('session.cookie_httponly', 'On');
 ini_set('session.cookie_lifetime', '14400');
 ini_set('session.gc_maxlifetime', '86400');
 ini_set('session.gc_probability', '1');
-ini_set('session.cookie_secure', 'Off');
+ini_set('session.cookie_secure', 'On');
 
 error_reporting(E_ALL | E_STRICT);
 
