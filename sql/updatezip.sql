1bdd1bb4be8cbbf279f073c8e66a81a34c3e4eb4
Alan Griggs <alangriggs219@gmail.com>
2018-03-02 22:17:53 -0600

    Slightly more echo statements to improve logging in zip reference update

--- sql/updatezip.sql
+++ sql/updatezip.sql
@@ -1,9 +1,10 @@
+SELECT 'Starting number of zip codes: ', COUNT(*) FROM postal_code_ref;
 SET FOREIGN_KEY_CHECKS=0;
 START TRANSACTION;
 DELETE FROM postal_code_ref;
 LOAD DATA LOCAL INFILE 'sorted.txt' INTO TABLE postal_code_ref
-	(country_code, postal_code, place_name, admin_name1, admin_code1, admin_name2,
-	admin_code2, admin_name3, admin_code3, latitude, longitude);
+        (country_code, postal_code, place_name, admin_name1, admin_code1, admin_name2,
+        admin_code2, admin_name3, admin_code3, latitude, longitude);
 COMMIT;
 SET FOREIGN_KEY_CHECKS=1;
-
+SELECT 'Ending number of zip codes: ', COUNT(*) FROM postal_code_ref;
